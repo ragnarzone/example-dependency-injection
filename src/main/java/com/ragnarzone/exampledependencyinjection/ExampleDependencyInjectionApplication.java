df94075ff78bc7211c06711354a354190dc6f9b9
ragnarzone <ragnarzone@gmail.com>
2022-08-07 11:11:47 +0300

    added example for inversion of control with spring context

--- src/main/java/com/ragnarzone/exampledependencyinjection/ExampleDependencyInjectionApplication.java
+++ src/main/java/com/ragnarzone/exampledependencyinjection/ExampleDependencyInjectionApplication.java
@@ -1,13 +1,21 @@
 package com.ragnarzone.exampledependencyinjection;
 
+import com.ragnarzone.exampledependencyinjection.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ExampleDependencyInjectionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExampleDependencyInjectionApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(ExampleDependencyInjectionApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
 	}
 
 }
