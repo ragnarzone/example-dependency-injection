8546c33dfd86928caa86850dc6f6d5faf16d79bf
ragnarzone <ragnarzone@gmail.com>
2022-08-07 10:17:47 +0300

    Initial Commit

---
+++ src/main/java/com/ragnarzone/exampledependencyinjection/ExampleDependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.ragnarzone.exampledependencyinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleDependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleDependencyInjectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ragnarzone/exampledependencyinjection/ExampleDependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ragnarzone.exampledependencyinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleDependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
