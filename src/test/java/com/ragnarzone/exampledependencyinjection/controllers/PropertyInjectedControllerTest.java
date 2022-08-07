638ed4d38608fe63727dea52cc2d475e9f3876b3
ragnarzone <ragnarzone@gmail.com>
2022-08-07 12:25:16 +0300

    added three different services for different controllers and specified for each with @Qualified annotation

--- src/test/java/com/ragnarzone/exampledependencyinjection/controllers/PropertyInjectedControllerTest.java
+++ src/test/java/com/ragnarzone/exampledependencyinjection/controllers/PropertyInjectedControllerTest.java
@@ -1,10 +1,8 @@
 package com.ragnarzone.exampledependencyinjection.controllers;
 
-import com.ragnarzone.exampledependencyinjection.services.GreetingServiceImpl;
+import com.ragnarzone.exampledependencyinjection.services.ConstructorGreetingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class PropertyInjectedControllerTest {
 
@@ -14,7 +12,7 @@ class PropertyInjectedControllerTest {
     void setUp() {
         controller = new PropertyInjectedController();
 
-        controller.greetingService = new GreetingServiceImpl();
+        controller.greetingService = new ConstructorGreetingService();
     }
 
     @Test
--- src/test/java/com/ragnarzone/exampledependencyinjection/controllers/SetterInjectedControllerTest.java
+++ src/test/java/com/ragnarzone/exampledependencyinjection/controllers/SetterInjectedControllerTest.java
@@ -1,10 +1,8 @@
 package com.ragnarzone.exampledependencyinjection.controllers;
 
-import com.ragnarzone.exampledependencyinjection.services.GreetingServiceImpl;
+import com.ragnarzone.exampledependencyinjection.services.ConstructorGreetingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class SetterInjectedControllerTest {
 
@@ -13,7 +11,7 @@ class SetterInjectedControllerTest {
     @BeforeEach
     void setUp() {
         controller = new SetterInjectedController();
-        controller.setGreetingService(new GreetingServiceImpl());
+        controller.setGreetingService(new ConstructorGreetingService());
     }
 
     @Test
