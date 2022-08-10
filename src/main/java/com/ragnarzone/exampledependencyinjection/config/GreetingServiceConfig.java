325ad17a1ad1efca428378e64743f796416e215c
ragnarzone <ragnarzone@gmail.com>
2022-08-10 07:59:34 +0300

    changed loading of properties to spring boot context

--- src/main/java/com/ragnarzone/exampledependencyinjection/config/GreetingServiceConfig.java
+++ src/main/java/com/ragnarzone/exampledependencyinjection/config/GreetingServiceConfig.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
 
-@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:di-config.xml")
 @Configuration
 public class GreetingServiceConfig {
